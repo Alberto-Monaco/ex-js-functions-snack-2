63abf8039a8b490894bb2abf8ee93fcb05ede99c
Alberto Monaco <alberto.monaco84@gmail.com>
2025-02-12 10:44:11 +0100

    complete snack5

--- snack5.js
+++ snack5.js
@@ -3,3 +3,14 @@
 // Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
 
 // Nota: Questa funzione creerà un loop infinito. Interrompilo manualmente o usa clearInterval() in un altro script.
+
+function stampaOgniSecondo(message) {
+	return function () {
+		setInterval(() => {
+			console.log(message)
+		}, 1000)
+	}
+}
+
+const printMessage = stampaOgniSecondo('Hello World!')
+printMessage()
