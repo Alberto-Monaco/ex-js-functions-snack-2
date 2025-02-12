3b8b3b0ebaf539b2ecadd75326c5f39d34a1c550
Alberto Monaco <alberto.monaco84@gmail.com>
2025-02-12 10:59:07 +0100

    complete snack7

--- snack7.js
+++ snack7.js
@@ -2,3 +2,16 @@
 // Crea una funzione che ferma un timer dopo un certo tempo
 // Scrivi una funzione eseguiEferma che accetta un messaggio, un tempo di avvio e un tempo di stop.
 // Il messaggio deve essere stampato a intervalli regolari, ma si deve fermare dopo il tempo di stop.
+
+function eseguiEferma(messaggio, tempoStart, tempoStop) {
+	let timer = setInterval(() => {
+		console.log(messaggio)
+	}, tempoStart)
+
+	setTimeout(() => {
+		clearInterval(timer)
+		console.log('Timer fermato')
+	}, tempoStop)
+}
+
+eseguiEferma('Ciao', 1000, 5000)
