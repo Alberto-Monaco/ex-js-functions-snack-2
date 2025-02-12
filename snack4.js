a2022c4e74c87bdac0bb0a337f9bec9bee79df5b
Alberto Monaco <alberto.monaco84@gmail.com>
2025-02-12 10:38:21 +0100

    complete snack4

--- snack4.js
+++ snack4.js
@@ -2,3 +2,14 @@
 // Crea un generatore di funzioni creaTimer
 // Scrivi una funzione creaTimer che accetta un tempo (in ms) e
 // restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
+
+function creaTimer(time) {
+	return function () {
+		setTimeout(() => {
+			console.log('Tempo scaduto!')
+		}, time)
+	}
+}
+
+const timer1000 = creaTimer(1000)
+timer1000()
