527d2aec710b5913b8d9968716f36b3442ce6eb3
Alberto Monaco <alberto.monaco84@gmail.com>
2025-02-12 10:06:01 +0100

    fix text exercise

--- snack4.js
+++ snack4.js
@@ -1,3 +1,4 @@
 //? Snack 4
 // Crea un generatore di funzioni creaTimer
-// Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
+// Scrivi una funzione creaTimer che accetta un tempo (in ms) e
+// restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
