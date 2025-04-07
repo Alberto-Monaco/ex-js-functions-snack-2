b16f43c8fe077e561749459fd5b4fd3be7ae2b19
Alberto Monaco <alberto.monaco84@gmail.com>
2025-04-07 18:41:30 +0200

    add comment

--- snack1.js
+++ snack1.js
@@ -4,18 +4,21 @@
 // Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
 // Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
 
+// Funzione dichiarativa che somma due numeri
 function somma(a, b) {
 	return a + b
 }
 
-console.log(somma(2, 3))
+console.log(somma(2, 3)) // Output: 5
 
+// Funzione anonima assegnata a una variabile
 const somma2 = function (a, b) {
 	return a + b
 }
 
-console.log(somma2(7, 10))
+console.log(somma2(7, 10)) // Output: 17
 
+// Arrow function che somma due numeri
 const somma3 = (a, b) => a + b
 
-console.log(somma3(5, 5))
+console.log(somma3(5, 5)) // Output: 10
--- snack4.js
+++ snack4.js
@@ -3,13 +3,19 @@
 // Scrivi una funzione creaTimer che accetta un tempo (in ms) e
 // restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
 
+// Funzione che crea un timer personalizzato
 function creaTimer(time) {
+	// Restituisce una funzione (closure) che gestisce il timer
 	return function () {
+		// Imposta un timer che si attiva dopo il tempo specificato
 		setTimeout(() => {
+			// Stampa il messaggio quando il timer scade
 			console.log('Tempo scaduto!')
 		}, time)
 	}
 }
 
+// Crea un timer che si attiva dopo 1000 millisecondi (1 secondo)
 const timer1000 = creaTimer(1000)
+// Avvia il timer
 timer1000()
